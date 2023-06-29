32c768b2f887aeb804bc61e0d5f6099079bbd0ed
Carla <artazacarladaniela@gmail.com>
2023-06-29 10:37:18 -0300

    Se actualiza repo

--- src/main/java/com/lgf/portafolio/PortafolioApplication.java
+++ src/main/java/com/lgf/portafolio/PortafolioApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PortafolioApplication implements CommandLineRunner{
     
